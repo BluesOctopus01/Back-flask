3922e4f635577d77629e8311cba2ba1cefc8b813
BluesOctopus01 <metens.thomas@outlook.be>
2025-10-22 16:30:52 +0200

    initial commit

---
+++ app/models/__init__.py
@@ -0,0 +1,9 @@
+from app import db
+
+from .user import User
+from .deck import Deck
+from app.models.cards_models.card_answer_qcm import AnswerQcm
+from app.models.cards_models.card_gapfill import Gapfill
+from app.models.cards_models.card_image import Image
+from app.models.cards_models.card_qa import Card, Qa
+from app.models.cards_models.card_qcm import Qcm
