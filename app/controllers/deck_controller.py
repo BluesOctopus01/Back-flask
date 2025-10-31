7a59d1a5fd560c18fd6eb65f848e83ed133dcd86
BluesOctopus01 <metens.thomas@outlook.be>
2025-10-31 16:58:10 +0100

    Development (#8)

--- app/controllers/deck_controller.py
+++ app/controllers/deck_controller.py
@@ -1,5 +1,5 @@
 from app.models.deck import Deck
-from app.services.deck_service import post_deck
+from app.services.deck_service import post_deck, get_deck_user, get_deck, is_owner_deck
 from app.services.user_service import fetch_a_user
 from app.DTO.deck_dto import DeckCreateDTO
 from flask import jsonify, request
@@ -20,12 +20,26 @@ def create_deck_controller(user_id, data):
     return jsonify(response_data), 201
 
 
-def get_user_decks_controller():
-    pass
+def get_user_decks_controller(user_id):
+    creator = fetch_a_user(user_id)
+    if not creator:
+        return jsonify({"message": "user not found"}), 404
+
+    decks = get_deck_user(user_id)
+    if not decks:
+        return jsonify({"message": "decks not found"}), 404
+    decks_dict = [deck.to_dict() for deck in decks]
+    return jsonify(decks_dict), 200
 
 
-def get_deck_controller():
-    pass
+# todo retravailler ca pour une meilleure logique métier
+# def get_deck_controller(user_id, deck_id, data):
+#     deck = get_deck(deck_id, user_id)
+#     if not deck:
+#         return jsonify({"message": "deck not found"}), 404
+#     owner = is_owner_deck(user_id, deck_id)
+#     if not owner:
+#         pass
 
 
 def update_deck_controller():
---
+++ app/utils/data_utils.py
@@ -0,0 +1,58 @@
+PAYLOAD_DECK = {
+    "creator_id": 1,
+    "name": "Biologie Cellulaire",
+    "bio": "Deck pour réviser les bases de la biologie cellulaire.",
+    "access": "PROTECTED",
+    "image": "biologie_deck.png",
+    "access_key": "Bio2025!",
+}
+PAYLOAD_USER = {
+    "username": "Testeurfou13245",
+    "first_name": "TestFirst",
+    "last_name": "TestLast",
+    "password": "Test!123456",
+    "email": "test12345@gmail.com",
+    "gender": "M",
+    "phone_number": "047695872",
+    "birthdate": "1998-10-30",
+    "country": "Belgium",
+    "address": "Rue de feur, 56",
+    "user_bio": "Je test mon application tel un bon developper",
+    "image": "test.png",
+}
+PAYLOAD_LOGIN_USER = {"password": "Test!123456", "email": "test12345@gmail.com"}
+
+PAYLOAD_DECKS_WITH_ID1 = [
+    {
+        "creator_id": 1,
+        "name": "Biologie Cellulaire",
+        "bio": "Deck pour réviser les bases de la biologie cellulaire.",
+        "access": "PROTECTED",
+        "image": "biologie_deck.png",
+        "access_key": "Bio2025!",
+    },
+    {
+        "creator_id": 1,
+        "name": "Chimie Organique",
+        "bio": "Deck sur les réactions organiques fondamentales.",
+        "access": "PRIVATE",
+        "image": "chimie_deck.jpg",
+        "access_key": "ChemSecret42",
+    },
+    {
+        "creator_id": 1,
+        "name": "Physique Quantique",
+        "bio": "Introduction aux concepts de la mécanique quantique.",
+        "access": "PUBLIC",
+        "image": "quantique_deck.webp",
+        "access_key": None,
+    },
+    {
+        "creator_id": 1,
+        "name": "Histoire Médiévale",
+        "bio": "Deck sur les grandes dynasties du Moyen Âge.",
+        "access": "PROTECTED",
+        "image": "histoire_deck.png",
+        "access_key": "Medieval2025",
+    },
+]
